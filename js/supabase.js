bad7d8e0b3d3cbad79d2670e76a55de7e000e4b3
Fxandres30 <andres23.t30@gmail.com>
2026-01-30 14:23:28 -0500

    switch to new supabase project

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://fhdakfeyaxxzmuyvnci.supabase.co";
-const supabaseKey = "sb_publishable_SGQ5snuGTXnBq8_eHDghWQ_IWt8zOlU";
+const supabaseUrl = "https://slouziolnwtjbpxtpxvn.supabase.co";
+const supabaseKey = "sb_publishable_ps3hys_W0r7ZBv-wY7QAFw_PunRCj34";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
